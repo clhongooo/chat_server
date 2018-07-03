c91d7e6b87977988403638c83f6b78bd6e29bcd1
clhongooo <clhongooo@163.com>
2018-07-03 16:19:32 +0800

    add typhoon-blade into project

---
+++ chat_svr/main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: clhongooo
+	> Mail: clhongooo@163.com 
+	> Created Time: Tue 03 Jul 2018 04:12:51 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world!\n";
+	return 0;
+}
