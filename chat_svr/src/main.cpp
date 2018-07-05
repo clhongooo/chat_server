1496d4b470337cd8341c92ccf89c11ccc14ad319
clhongooo <clhongooo@163.com>
2018-07-05 15:48:39 +0800

    use blade-build to manage the structure of the project

--- chat_svr/src/main.cpp
+++ chat_svr/src/main.cpp
@@ -4,12 +4,20 @@
 	> Mail: clhongooo@163.com 
 	> Created Time: Tue 03 Jul 2018 04:12:51 PM CST
  ************************************************************************/
+#include <sys/ptrace.h>
+#include <libunwind.h>
+#include <libunwind-x86_64.h>
+#include <libunwind-ptrace.h>
 
 #include<iostream>
+#include<logging.h>
+
 using namespace std;
 
 int main()
 {
 	cout << "hello world!\n";
+	google::InitGoogleLogging("chat_svr_log");
+	LOG(INFO) << "test glog!";
 	return 0;
 }
