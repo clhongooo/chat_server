5d34fb77fd16e1ef76bd98f246eda6e55db5ced3
clhongooo <clhongooo@163.com>
2018-07-27 11:25:17 +0800

    scripts of building

--- build_allsvr.sh
+++ build_allsvr.sh
@@ -1,18 +1,26 @@
 #########################################################################
-# File Name: restart_all.sh
+# File Name: build_allsvr.sh
 # Author: clhongooo
 # mail: clhongooo@163.com
-# Created Time: Thu 26 Jul 2018 10:22:33 AM CST
+# Created Time: Fri 27 Jul 2018 11:14:31 AM CST
 #########################################################################
 #!/bin/bash
+
+cd chat_svr/src
+blade build -pdebug
+cd ../..
+
+cd robot/src
+blade build -pdebug
+cd ../..
+
 killall -9 chat_svr_debug
 killall -9 robot_svr_debug
 
 cd chat_svr/bin
 ./chat_svr_debug -log_dir=../log
-cd ../../
+cd ../..
 
 cd robot/bin
 ./robot_svr_debug -log_dir=../log
-cd ../../
-
+cd ../..
